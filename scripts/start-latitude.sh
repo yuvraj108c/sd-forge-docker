329098f599d86429ad475cb7081b94c42a0c26ef
yuvraj108c <yuvraj108c@gmail.com>
2024-06-24 17:06:22 +0000

    ssh

--- scripts/start-latitude.sh
+++ scripts/start-latitude.sh
@@ -1,5 +1,29 @@
 #!/bin/bash
 
+SSH_USER=${SSH_USER:-root}
+
+# Check if SSH_USER exists, if not create it
+if ! id "$SSH_USER" &>/dev/null; then
+    useradd -m $SSH_USER
+fi
+
+# Enable or disable password authentication based on SSH_PASSWORD_AUTH environment variable
+if [ -n "$SSH_PASSWORD" ]; then
+    sed -i 's/^PasswordAuthentication no/PasswordAuthentication yes/' /etc/ssh/sshd_config
+    echo "$SSH_USER:$SSH_PASSWORD" | chpasswd
+fi
+
+# If a PUBLIC_KEY environment variable is provided, add the key to the SSH_USER
+if [ -n "$PUBLIC_KEY" ]; then
+    # Determine correct home directory
+    HOME_DIR=$(getent passwd "$SSH_USER" | cut -d: -f6)
+    mkdir -p $HOME_DIR/.ssh
+    echo $PUBLIC_KEY > $HOME_DIR/.ssh/authorized_keys
+    chown -R $SSH_USER:$SSH_USER $HOME_DIR/.ssh
+    chmod 700 $HOME_DIR/.ssh
+    chmod 600 $HOME_DIR/.ssh/authorized_keys
+fi
+
 mkdir -p /workspace/stable-diffusion-webui-forge
 mv /temp/stable-diffusion-webui-forge/* /workspace/stable-diffusion-webui-forge
 mv /temp/stable-diffusion-webui-forge/.* /workspace/stable-diffusion-webui-forge
